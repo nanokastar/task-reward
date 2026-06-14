27ffe7e1cc63cf2aeab72b5dc34412605a6f2a55
nanokastar <newsnippon0415@gmail.com>
2026-06-14 16:22:04 +0800

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'task-reward-v1';
+const URLS_TO_CACHE = ['/'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          if (event.request.method === 'GET') cache.put(event.request, copy);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
